66efa2a89a747a1686656f9c88d0fc7241a6b57d
Tanishq Jain <tanishhhh38@gmail.com>
2019-01-16 15:56:54 +0530

    update

--- public/application.js
+++
@@ -1,3 +0,0 @@
-
-// here we register all the modules we are using in our angular app
-angular.module('adaptive-learning',[]);
--- public/routes/client.route.js
+++
@@ -1,7 +0,0 @@
-
-angular.module('adaptive-learning').config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider){
-
-    $routeProvider.when('/',{
-        templateUrl : 'public/views/googlelogin.client.view.html'
-    })
-}])
